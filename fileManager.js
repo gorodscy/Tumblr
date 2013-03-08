daa9c8c0f4a692781e6b70d9288f2c5377161fdc
Gorodscy <fefow@me.com>
2013-03-08 13:54:51 -0500

    fileManager.js updated to support several blogs

--- fileManager.js
+++ fileManager.js
@@ -2,8 +2,73 @@
 // OBS: Other files should evoke require('./fileManager.js'); (if in the same dir)
 module.exports.writePosts = writePosts;
 module.exports.readPosts = readPosts;
+module.exports.saveBlog = saveBlog;
+module.exports.readBlogs = readBlogs;
 
-var fs = require('fs');
+var blog_count = 0;
+
+// Write blog track list
+function saveBlog(blog_list, url){
+	var fs = require('fs');
+	
+	var string = '{"count":' + (blog_count+1) + ',';
+	// If first blog saved
+	if(blog_count == 0){
+	 	string += '"blog":["' + url + '"]}';
+	 }
+	 else {
+	 	for(var i=0; i<=blog_count; i++){
+	 		// If first URL
+			if(i==0){
+				string += '"blog":[' + JSON.stringify(blog_list.blog[i]);
+			}
+			// If last URL
+			else if(i==blog_count){
+				string += ',"' + url + '"]}';
+			}
+			// Otherwise
+			else {
+				string += ',' + JSON.stringify(blog_list.blog[i]);
+			}
+	 	}
+	 }
+	
+	fs.writeFile('blogs.txt', string, function (err) {
+		if(err) throw err;
+	});
+	
+	// Update blog_list:
+	blog_list = JSON.parse(string);
+	
+	blog_count += 1;
+	return JSON.parse(string);
+}
+
+// Read blog track list
+function readBlogs(blog_list){
+	var fs = require('fs');
+	
+	fs.readFile('blogs.txt', function (err, data) {
+		if(err) throw err;
+		
+		var count = 0;
+		var j = JSON.parse(data, reviver);
+		
+		// Function to count how many blogs has been tracked
+		function reviver(key, value){
+			if(typeof value === 'string'){
+				count += 1;
+			}
+			return value;
+		}
+		
+		blog_list = j;
+		
+		//console.log(JSON.stringify(blog_list.blog[0]));
+		return count;
+		
+	});
+}
 
 // Writing many posts in a file:
 function writePosts(posts){
@@ -29,36 +94,39 @@ function writePosts(posts){
 
 // Reading posts from a file:
 function readPosts(){
+	
+	var fs = require('fs');
+	
 	fs.readFile('posts.txt', function (err, data) {
-		if (err) throw err; {
-			var j = JSON.parse(data); 
-			
-			var posts = j.response.liked_posts;
-			var liked = j.response.liked_count;
-			// For each post liked by the user (Limited to the last 20 posts)
-			for(var i=0; i<liked && i<20; i++){
+		if (err) throw err; 
+		var j = JSON.parse(data); 
+		
+		var posts = j.response.liked_posts;
+		var liked = j.response.liked_count;
+		// For each post liked by the user (Limited to the last 20 posts)
+		for(var i=0; i<liked && i<20; i++){
 
-				/// Display post information:
+			/// Display post information:
 
-				console.info('#-------------------------------------#');
-				console.info('Post #', i, ':');
-	
-				console.info('>>	URL: ', posts[i].post_url);
-				console.info('>>	DATE: ', posts[i].date);
-				if(posts[i].type == 'photo') {
-					console.info('>>	PHOTO: ', posts[i].photos);
-				} else if(posts[i].type == 'text') {
-					console.info('>>	TITLE: ', posts[i].title);
-					console.info('>>	TEXT: ', posts[i].body);
-				}
-				console.info('>>	NOTE COUNT (likes): ', posts[i].note_count);
-	
-				// If this is the last post
-				if(i == liked-1)
-					console.info('#-------------------------------------#');
-				
-				/// Finish displaying post information
+			console.info('#-------------------------------------#');
+			console.info('Post #', i, ':');
+
+			console.info('>>	URL: ', posts[i].post_url);
+			console.info('>>	DATE: ', posts[i].date);
+			if(posts[i].type == 'photo') {
+				console.info('>>	PHOTO: ', posts[i].photos);
+			} else if(posts[i].type == 'text') {
+				console.info('>>	TITLE: ', posts[i].title);
+				console.info('>>	TEXT: ', posts[i].body);
 			}
+			console.info('>>	NOTE COUNT (likes): ', posts[i].note_count);
+
+			// If this is the last post
+			if(i == liked-1)
+				console.info('#-------------------------------------#');
+			
+			/// Finish displaying post information
 		}
+		
 	});
 }
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var tracker = require('./tracker.js');
+var file = require('./fileManager.js');
 
 // Lauch the tracker as soon as the server begin
-tracker.trackBlog('https://api.tumblr.com/v2/blog/gorodscy.tumblr.com/likes?\
-api_key=ZtJYLO0HI9tPYsC2pqCy6ciItK3XxWL9KgQErmo2TsknKtNtEp');
+everyhourFunction();
 
 // Making a function be executed every hour:
 var interval = 3600000; // 1 hour in milliseconds
@@ -10,9 +10,17 @@ var interval = 3600000; // 1 hour in milliseconds
 var runningFunction = setInterval(everyhourFunction, interval);
 
 function everyhourFunction() {
-  // Code to be executed:
-  tracker.trackBlog('https://api.tumblr.com/v2/blog/gorodscy.tumblr.com/likes?\
+	// Code to be executed:
+
+	var blog_list;
+	var count;
+
+  	blog_list = file.saveBlog(blog_list, 'https://api.tumblr.com/v2/blog/gorodscy.tumblr.com/likes?\
 api_key=ZtJYLO0HI9tPYsC2pqCy6ciItK3XxWL9KgQErmo2TsknKtNtEp');
+	blog_list = file.saveBlog(blog_list, 'https://api.tumblr.com/v2/blog/ystallonne.tumblr.com/likes?\
+api_key=ZtJYLO0HI9tPYsC2pqCy6ciItK3XxWL9KgQErmo2TsknKtNtEp');
+  
+ 	 count = file.readBlogs(blog_list);
 }
 
 // [If necessary] Removing the running condition:
