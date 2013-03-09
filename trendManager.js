6d5e2863e93658a8a9f33ba620003d754da4021f
Vanessa Lima <vanny.a.lima@gmail.com>
2013-03-09 18:20:17 -0300

    Update functions

--- trendManager.js
+++ trendManager.js
@@ -11,6 +11,7 @@ function trendBlog (req, res){
 
 	console.log("Retrieve the trends for ", hostname);	
 
+	// Return an error if the order does not match
 	if(order != 'Trending' | order != 'Recent') {
 		console.log("URL is wrong");
 		res.send(404);
@@ -19,21 +20,20 @@ function trendBlog (req, res){
 
 	db.getAllPosts(blog.hostname, function(posts) {
 		console.log();
-		// EXEMPLO ****
-//***		db.getLastTrack(posts[0], function(track){
-//***			
-//***			console.log(track);
-//***			
-//***		});
 
-		// Order by the difference of likes
+		// Order by the the date of like
 		if(order == 'Recent'){
+			
+			db.getPreviousTrack(post[i], function(track) { 
+				
+			});
+
 			// Send JSON to the client
 			res.on('data', function(posts){
 				console.log('Sending json back');
 			});				
 		}
-		// Order by the the date of like
+		// Order by the difference of likes
 		else {
 			// Send JSON to the client
 			res.on('data', function(posts){
@@ -50,6 +50,7 @@ function trendAll (req, res){
 	
 	console.log("Retrieve all trends being tracked");
 
+	// Return an error if the order does not match
 	if(order != 'Trending' | order != 'Recent') {
 		console.log("URL is wrong");
 		res.send(404);
@@ -60,12 +61,12 @@ function trendAll (req, res){
 		var i =0;
 		for(var i = 0; blogs[i] != undefined; i++) {
 			db.getAllPosts(blogs[i].hostname, function(posts) {
+				// Order by the the date of like
 				if(order == 'Recent'){
-					// Order by the difference of likes
 					
 				}
+				// Order by the difference of likes
 				else {
-					// Order by the the date of like
 					
 				}
 			});
