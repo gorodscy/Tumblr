a964170a1174bbc04a494aa9b9e96ec06dbbd6fd
Gorodscy <fefow@me.com>
2013-03-08 23:00:24 -0500

    statusCode fixed [Thanks to Ystallonne]

--- tracker.js
+++ tracker.js
@@ -14,19 +14,22 @@ function postBlog(req, res){
 	var url = 'https://api.tumblr.com/v2/blog/' + hostname + '/likes?\
 api_key=ZtJYLO0HI9tPYsC2pqCy6ciItK3XxWL9KgQErmo2TsknKtNtEp';
 
-	res.send(trackBlog(url));
+	trackBlog(url, function(s){
+		res.send(s);
+	});
+	
 }
 
-function trackBlog(url){
+function trackBlog(url, cb){
 	
 	var https = require('https');
 	var file = require('./fileManager.js');
-	var status;
 	
 	// do the GET request
 	var get_request = https.get(url, function(res) {
 		
-		status = res.statusCode;
+		// Callback function to send the statusCode to the POST request
+		cb(res.statusCode);
 		
 		// Create a variable to accumulate data.
 		// It is necessary because the data could be to long.
@@ -42,7 +45,7 @@ function trackBlog(url){
 		res.on('end', function () {
 			
 			// Create the JSON only if the hostname is valid.
-			if(statusCode == 200){
+			if(status == 200){
 				// Parse the data to JSON Object
 				var j = JSON.parse(data);
 		
@@ -59,8 +62,6 @@ function trackBlog(url){
 	get_request.on('error', function(e) {
 		console.error(e);
 	});
-	// Return 200 if tumbler user exists. 404 Otherwise.
-	return status;
 
 }
 
