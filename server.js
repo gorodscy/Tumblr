126812445aede7670a5fc2c0b0c49306ea85307e
Gorodscy <fefow@me.com>
2013-03-11 14:45:30 -0400

    Assignment DONE

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ express = require('express');
 var port = 31010;
 
 // Making a function be executed every hour:
-var interval = 400000;//3600000; // 1 hour in milliseconds
+var interval = 3600000; // 1 hour in milliseconds
 
 // Create an express application
 var app = express();
